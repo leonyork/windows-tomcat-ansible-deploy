01b5421bc3c0d9177f9348ffa038ddb2d784c55e
Leon York <leon@leonyork.com>
2019-12-28 01:25:43 +0000

    Fix travis

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 make .deploy
 # Sleep for a bit - i.e. Recommended time from AWS for Windows to come up - 4 minutes
 # This has failed a few times, so upping to 5 mins
-sleep 300
-HOST=$(make .deploy-output | jq '.public_ip.value' -rj) \
-PASSWORD=$(make .deploy-output | jq '.password.value' -rj) \
+sleep 360
+HOST=$(docker-compose -f deploy.docker-compose.yml -p windows-tomcat-ansible-deploy run --entrypoint 'terraform output --json' deploy | jq '.public_ip.value' -rj) \
+PASSWORD=$(docker-compose -f deploy.docker-compose.yml -p windows-tomcat-ansible-deploy run --entrypoint 'terraform output --json' deploy | jq '.password.value' -rj) \
 docker-compose -f update.docker-compose.yml run update "$@"
