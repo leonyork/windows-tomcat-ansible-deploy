cac76e456fae4d82137e9f4a595d4ace7ac4c8aa
Leon York <leon@leonyork.com>
2019-12-28 13:57:58 +0000

    Up timeout to try and fix Travis

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 # !/usr/bin/env sh
 HOST=$(docker-compose -f deploy.docker-compose.yml -p windows-tomcat-ansible-deploy run --entrypoint 'terraform output --json' deploy | jq '.public_ip.value' -rj)
-docker run --rm curlimages/curl:7.67.0 -L -m 5 -v http://$HOST:8080/
+docker run --rm curlimages/curl:7.67.0 -L -m 10 -v http://$HOST:8080/
